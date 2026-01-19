4e755a31fe40d8224394e1290bac6194f37b24ce
mo171 <movindsouza79@gmail.com>
2026-01-19 23:42:28 +0530

    frontend deployment ready

--- frontend/src/components/ui/calendar.tsx
+++ frontend/src/components/ui/calendar.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
+import "react-day-picker/style.css";
 
 import { cn } from "./utils";
 import { buttonVariants } from "./button";
@@ -60,12 +61,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
+        Chevron: ({ orientation, ...props }) => {
+          const Icon = orientation === "left" ? ChevronLeft : ChevronRight;
+          return <Icon className="size-4" {...props} />;
+        },
       }}
       {...props}
     />
